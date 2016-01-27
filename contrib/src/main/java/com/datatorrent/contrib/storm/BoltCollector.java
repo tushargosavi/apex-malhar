b5cf628fd6bbc0f2814a238da7f6910a6fdcf9f1
shubham <shubham-pathak22@github.com>
2016-01-27 15:26:24 +0530

    bolt

--- contrib/src/main/java/com/datatorrent/contrib/storm/BoltCollector.java
+++ contrib/src/main/java/com/datatorrent/contrib/storm/BoltCollector.java
@@ -10,17 +10,24 @@ import backtype.storm.tuple.Tuple;
 
 public class BoltCollector extends DefaultOutputPort implements IOutputCollector
 {
+  
+  private final DefaultOutputPort out;
 
+  
+  public BoltCollector(){
+    this.out=new DefaultOutputPort<>();
+  }
   @Override
-  public List<Integer> emit(String s, Collection<Tuple> collection, List<Object> list)
+  public List<Integer> emit(final String streamId, final Collection<Tuple> anchors, final List<Object> tuple)
   {
+    out.emit(tuple);
     return null;
   }
 
   @Override
   public void emitDirect(int i, String s, Collection<Tuple> collection, List<Object> list)
   {
-
+    throw new UnsupportedOperationException("Direct emit is not supported by Apex");
   }
 
   @Override
--- contrib/src/main/java/com/datatorrent/contrib/storm/BoltWrapper.java
+++ contrib/src/main/java/com/datatorrent/contrib/storm/BoltWrapper.java
@@ -1,10 +1,42 @@
 package com.datatorrent.contrib.storm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.datatorrent.api.Context;
+import com.datatorrent.api.DefaultInputPort;
 import com.datatorrent.api.Operator;
+
+import backtype.storm.generated.StormTopology;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.tuple.Tuple;
 
 public class BoltWrapper implements Operator
 {
+  
+  private final transient IRichBolt bolt;
+  private Map config = new HashMap();
+  private final String name;
+  private StormTopology stormTopology;
+
+  public BoltWrapper(final IRichBolt bolt, String name) throws IllegalArgumentException {
+    this.bolt = bolt;
+    this.name = name;
+  }
+  
+  private final transient BoltCollector outputCollector = new BoltCollector();
+  
+  private final transient DefaultInputPort<Tuple> input = new DefaultInputPort<Tuple>(){
+
+    @Override
+    public void process(Tuple tuple)
+    {
+      bolt.execute(tuple);
+    }
+    
+  };
   @Override
   public void beginWindow(long l)
   {
@@ -20,12 +52,15 @@ public class BoltWrapper implements Operator
   @Override
   public void setup(Context.OperatorContext context)
   {
+    OutputCollector stormCollector = new OutputCollector(outputCollector); 
+    final TopologyContext topologyContext = Helper.createTopologyContext(context, this.bolt, this.name, this.stormTopology, config);
+    this.bolt.prepare(config, topologyContext, stormCollector);
 
   }
 
   @Override
   public void teardown()
   {
-
+    this.bolt.cleanup();
   }
 }
